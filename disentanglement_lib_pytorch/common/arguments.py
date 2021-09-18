b1e23d8acb94fce72dab920bee56ec80f0257525
Muhammad Yaseen <mohammad_yaseen13@live.com>
2021-09-18 17:14:56 +0200

    visdom port added

--- disentanglement_lib_pytorch/common/arguments.py
+++ disentanglement_lib_pytorch/common/arguments.py
@@ -119,6 +119,7 @@ def get_args(sys_args):
     parser.add_argument('--traverse_l', default=False, type=str2bool, help='whether to traverse the l space')
     parser.add_argument('--traverse_c', default=False, type=str2bool, help='whether to traverse the condition')
     parser.add_argument('--verbose', default=20, type=int, help='verbosity level')
+    parser.add_argument('--visdom_port', default=8097, type=int, help='Visdom port to start server on')
 
     # Save/Load checkpoint
     parser.add_argument('--ckpt_dir', default='checkpoints', type=str, help='checkpoint directory')
